1e5fe3ddd3fc7c28ac1fa58a764b00f308317c28
Kilian <k.leybros.15@gmail.com>
2024-03-15 12:34:00 +0100

    Implementing auth

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,16 +11,26 @@ java {
 	sourceCompatibility = JavaVersion.VERSION_17
 }
 
+configurations {
+	compileOnly {
+		extendsFrom(configurations.annotationProcessor.get())
+	}
+}
+
 repositories {
 	mavenCentral()
 }
 
 dependencies {
+	implementation("io.jsonwebtoken:jjwt-api:0.12.5")
+	implementation("io.jsonwebtoken:jjwt-impl:0.12.5")
+	implementation("io.jsonwebtoken:jjwt-jackson:0.12.5")
 	implementation("org.springframework.boot:spring-boot-starter-security")
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	//implementation("org.springframework.boot:spring-boot-configuration-processor:3.2.3")
 	runtimeOnly("com.mysql:mysql-connector-j")
+	annotationProcessor("org.projectlombok:lombok")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
---
+++ src/main/java/com/openclassrooms/chatop/model/entity/User.java
@@ -0,0 +1,98 @@
+package com.openclassrooms.chatop.model.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.experimental.Accessors;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.sql.Timestamp;
+import java.util.Collection;
+
+
+@Data
+@Entity
+@Table(name = "users", indexes = @Index(columnList = "email"))
+public class User implements UserDetails {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name="created_at")
+    private Timestamp createdAt;
+
+    @Column(name="updated_at")
+    private Timestamp updatedAt;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getPassword() {
+        return this.password;
+    }
+
+    @Override
+    public String getUsername() {
+        return this.email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+
+
+    public User setEmail(String email) {
+        this.email = email;
+        return this;
+    }
+
+    public User setName(String name) {
+        this.name = name;
+        return this;
+    }
+
+    public User setPassword(String password) {
+         this.password = password;
+        return this;
+    }
+
+    public User setCreatedAt(Timestamp createdAt) {
+         this.createdAt = createdAt;
+        return this;
+    }
+
+    public User setUpdatedAt(Timestamp updatedAt) {
+         this.updatedAt = updatedAt;
+        return this;
+    }
+}
