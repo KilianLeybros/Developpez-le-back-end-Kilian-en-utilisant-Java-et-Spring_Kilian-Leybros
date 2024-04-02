fd2f434c2d1c140956221f1f2d771d4b3dd8deab
Kilian <k.leybros.15@gmail.com>
2024-04-02 18:48:55 +0200

    Mise à jour script sql

--- src/main/resources/sql/init.sql
+++ src/main/resources/sql/init.sql
@@ -1,25 +1,8 @@
--- MySQL Workbench Forward Engineering
 
-SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
-SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
-SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
+CREATE SCHEMA IF NOT EXISTS `chatop`;
+USE `chatop`;
 
--- -----------------------------------------------------
--- Schema mydb
--- -----------------------------------------------------
--- -----------------------------------------------------
--- Schema chatop
--- -----------------------------------------------------
 
--- -----------------------------------------------------
--- Schema chatop
--- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `chatop` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci ;
-USE `chatop` ;
-
--- -----------------------------------------------------
--- Table `chatop`.`users`
--- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `chatop`.`users` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `email` VARCHAR(255) NULL DEFAULT NULL,
@@ -28,16 +11,9 @@ CREATE TABLE IF NOT EXISTS `chatop`.`users` (
   `created_at` TIMESTAMP NULL DEFAULT NULL,
   `updated_at` TIMESTAMP NULL DEFAULT NULL,
   PRIMARY KEY (`id`),
-  UNIQUE INDEX `USERS_index` (`email` ASC) VISIBLE)
-ENGINE = InnoDB
-AUTO_INCREMENT = 20
-DEFAULT CHARACTER SET = utf8mb4
-COLLATE = utf8mb4_0900_ai_ci;
+  UNIQUE INDEX `USERS_index` (`email`));
 
 
--- -----------------------------------------------------
--- Table `chatop`.`rentals`
--- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `chatop`.`rentals` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `name` VARCHAR(255) NULL DEFAULT NULL,
@@ -49,19 +25,12 @@ CREATE TABLE IF NOT EXISTS `chatop`.`rentals` (
   `created_at` TIMESTAMP NULL DEFAULT NULL,
   `updated_at` TIMESTAMP NULL DEFAULT NULL,
   PRIMARY KEY (`id`),
-  INDEX `owner_id` (`owner_id` ASC) VISIBLE,
+  INDEX `owner_id` (`owner_id`),
   CONSTRAINT `rentals_ibfk_1`
     FOREIGN KEY (`owner_id`)
-    REFERENCES `chatop`.`users` (`id`))
-ENGINE = InnoDB
-AUTO_INCREMENT = 15
-DEFAULT CHARACTER SET = utf8mb4
-COLLATE = utf8mb4_0900_ai_ci;
+    REFERENCES `chatop`.`users` (`id`));
 
 
--- -----------------------------------------------------
--- Table `chatop`.`messages`
--- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `chatop`.`messages` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `rental_id` INT NULL DEFAULT NULL,
@@ -70,20 +39,13 @@ CREATE TABLE IF NOT EXISTS `chatop`.`messages` (
   `created_at` TIMESTAMP NULL DEFAULT NULL,
   `updated_at` TIMESTAMP NULL DEFAULT NULL,
   PRIMARY KEY (`id`),
-  INDEX `user_id` (`user_id` ASC) VISIBLE,
-  INDEX `rental_id` (`rental_id` ASC) VISIBLE,
+  INDEX `user_id` (`user_id`),
+  INDEX `rental_id` (`rental_id`),
   CONSTRAINT `messages_ibfk_1`
     FOREIGN KEY (`user_id`)
     REFERENCES `chatop`.`users` (`id`),
   CONSTRAINT `messages_ibfk_2`
     FOREIGN KEY (`rental_id`)
-    REFERENCES `chatop`.`rentals` (`id`))
-ENGINE = InnoDB
-AUTO_INCREMENT = 5
-DEFAULT CHARACTER SET = utf8mb4
-COLLATE = utf8mb4_0900_ai_ci;
+    REFERENCES `chatop`.`rentals` (`id`));
 
 
-SET SQL_MODE=@OLD_SQL_MODE;
-SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
-SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
